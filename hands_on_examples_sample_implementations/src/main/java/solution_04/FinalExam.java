2345bc458a6f1d503ca8cb43388acb4bcaaa078f
Nico Waldispühl <nw@retorte.ch>
2015-03-09 21:44:18 +0100

    Some improvements

--- hands_on_examples_sample_implementations/src/main/java/solution_04/FinalExam.java
+++ hands_on_examples_sample_implementations/src/main/java/solution_04/FinalExam.java
@@ -10,17 +10,17 @@ public class FinalExam {
 	
 	private ArrayList<Student> applicants = new ArrayList<>();
 	
+	public void applyForFinalExam(Student student) {
+		applicants.add(student);
+		System.out.println("Added to the final exam applicants list: " + student.getName());
+	}
+	
 	/**
 	 * A student is eligible for the final exam if and only if:
 	 * He or she is older than 23.
 	 */
 	public boolean isEligibleForFinalExam(Student student) {
 		return 23 < student.getAgeIn(currentYear);
-	}
-	
-	public void applyForFinalExam(Student student) {
-		applicants.add(student);
-		System.out.println("Added to the final exam applicants list: " + student.getName());
 	}
 
 	public void printAcceptedApplicants() {
