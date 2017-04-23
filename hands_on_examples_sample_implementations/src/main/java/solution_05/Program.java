a74899bda9c73bd0ec95174f205a666e59a8430a
Nico Waldispühl <nw@retorte.ch>
2017-04-23 16:54:16 +0200

    Updated years in final exam example.

--- hands_on_examples_sample_implementations/src/main/java/solution_05/Program.java
+++ hands_on_examples_sample_implementations/src/main/java/solution_05/Program.java
@@ -5,12 +5,12 @@ public class Program {
 	public static void main(String[] args) {
 		
 		FinalExam finalExam = new FinalExam();
-		finalExam.applyForFinalExam(new Student("678901", "Rosie", 1989));
-		finalExam.applyForFinalExam(new Student("123456", "Paula", 1990));
-		finalExam.applyForFinalExam(new Student("234567", "Walter", 1991));
-		finalExam.applyForFinalExam(new Student("345678", "Brenda", 1992));
-		finalExam.applyForFinalExam(new Student("456789", "Chuck", 1993));
-		finalExam.applyForFinalExam(new Student("567890", "Dan", 1994));
+		finalExam.applyForFinalExam(new Student("678901", "Rosie", 1991));
+		finalExam.applyForFinalExam(new Student("123456", "Paula", 1992));
+		finalExam.applyForFinalExam(new Student("234567", "Walter", 1993));
+		finalExam.applyForFinalExam(new Student("345678", "Brenda", 1994));
+		finalExam.applyForFinalExam(new Student("456789", "Chuck", 1995));
+		finalExam.applyForFinalExam(new Student("567890", "Dan", 1996));
 		
 		// The following statements should print a line for every student which is eligible for the final exam like this:
 		// Accepted student: Rosie (678901)
